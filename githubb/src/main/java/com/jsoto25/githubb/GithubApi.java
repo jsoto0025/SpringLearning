049636c269e3b91f4a7a2d9b1c098978dc04849d
jsoto25@hotmail.com <jsoto25@hotmail.com>
2020-03-30 05:39:25 -0500

    JSON Libraries

--- githubb/src/main/java/com/jsoto25/githubb/GithubApi.java
+++ githubb/src/main/java/com/jsoto25/githubb/GithubApi.java
@@ -2,7 +2,11 @@ package com.jsoto25.githubb;
 
 import javax.websocket.server.PathParam;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.sym.Name;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 //import com.google.gson.Gson;
 import org.springframework.boot.autoconfigure.*;
@@ -18,8 +22,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableAutoConfiguration
 public class GithubApi {
 
-  private static String githubToken = "4c97eac7fa51ffd034a223f566b8ed7e045eca9b";
-  private static String gitHubApiUrl = "https://api.github.com/";
+  private final String githubToken = "4c97eac7fa51ffd034a223f566b8ed7e045eca9b";
+  private final String gitHubApiUrl = "https://api.github.com/";
 
   @CrossOrigin
   @RequestMapping("/")
@@ -28,16 +32,16 @@ public class GithubApi {
   }
 
   @GetMapping("/repos")
-  String getListRepos(@RequestParam String gitHubUser) {
+  String getListRepos(@RequestParam final String gitHubUser) {
 
-    String result = "Getting repos list of: " + gitHubUser;
+    final String result = "Getting repos list of: " + gitHubUser;
 
     return result;
 
   }
 
   @GetMapping("/reposfinal")
-  String getListReposFinal(@RequestParam String gitHubUser) {
+  String getListReposFinal(@RequestParam final String gitHubUser) throws JsonMappingException, JsonProcessingException {
 
     String result = "Getting repos list of: " + gitHubUser;
     String repositoryListsUrl = this.gitHubApiUrl + "/users/" + gitHubUser + "/repos";
@@ -54,25 +58,30 @@ public class GithubApi {
      * 
      * return result;
      */
-    RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<String> response = restTemplate.getForEntity(repositoryListsUrl, String.class);
+    final RestTemplate restTemplate = new RestTemplate();
+    final ResponseEntity<String> response = restTemplate.getForEntity(repositoryListsUrl, String.class);
     // assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
-    return response.getBody();
+
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode root = mapper.readTree(response.getBody());
+    //final JsonNode name = root.path("name");
+
+    return root.toPrettyString();
   }
 
   @PostMapping("/create")
 
-  String newEmployee(@RequestBody Repo newRepo) {
+  String newEmployee(@RequestBody final Repo newRepo) {
 
-    RestTemplate restTemplate = new RestTemplate();
-    HttpHeaders headers = new HttpHeaders();
+    final RestTemplate restTemplate = new RestTemplate();
+    final HttpHeaders headers = new HttpHeaders();
     headers.add("Authorization", "Bearer " + githubToken);
     headers.add("Content-Type", "application/json");
     headers.add("Content-Type", "application/json");
     // String jsonRepository = new Gson().toJson(newRepo);
-    String jsonRepository = "new Gson().toJson(newRepo)";
-    HttpEntity<String> entity = new HttpEntity<String>(jsonRepository, headers);
-    String result = restTemplate.postForObject("https://api.github.com/user/repos", entity, String.class);
+    final String jsonRepository = "new Gson().toJson(newRepo)";
+    final HttpEntity<String> entity = new HttpEntity<String>(jsonRepository, headers);
+    final String result = restTemplate.postForObject("https://api.github.com/user/repos", entity, String.class);
 
     return result;
   }
